0c2c2598a405292bcf122c0de4b6498ab1a28cb8
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2021-11-30 16:51:02 +0100

    build config changed

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 var path = require("path");
 module.exports = {
+  outputDir: path.resolve(__dirname, "./build"),
   configureWebpack: {
     resolve: {
       modules: [path.resolve("./src"), path.resolve("./node_modules")],
