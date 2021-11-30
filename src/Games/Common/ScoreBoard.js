aedd9aedff2cd1c291642b38dfba1626c21677ea
Jaikumar.T.J <jaikumar.jayapal@commercetools.com>
2021-11-30 15:29:50 +0100

    clean up

--- src/Games/Common/ScoreBoard.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./ScoreBoard.css";
-const ScoreBoard = ({ score, total }) => {
-  return <div className={"board"}>{`${score} / ${total}`}</div>;
-};
-
-export default ScoreBoard;
